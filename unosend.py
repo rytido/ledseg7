bfc806c560815caa1a017e433181418b13843155
ryan <rtdood@mtu.edu>
2019-01-26 19:47:21 -0500

    comment about how to do equivalent in command line

--- unosend.py
+++ unosend.py
@@ -1,10 +1,13 @@
 from time import sleep
 import serial
 
+# equivalent to
+# echo 345 > /dev/ttyACM0
+
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
 while True:
-    sleep(3)
+    sleep(2)
     ser.write(b'345\n')
     sleep(2)
     ser.write(b'454556\n')
